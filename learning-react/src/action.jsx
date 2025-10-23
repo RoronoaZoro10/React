cd93b887b9b03f5e64afe441cd12ab453176d404
RoronoaZoro10 <revan28710@gmail.com>
2025-10-23 21:58:07 +0530

    23 oct 2025

---
+++ learning-react/src/action.jsx
@@ -0,0 +1,14 @@
+import action from './assets/action.jpeg'
+
+function Action()
+{
+    return(
+        <>
+        <h1>Welcome to the anime world</h1>
+        <img src={action}></img>
+        <h6>Action</h6>
+        </>
+    )
+}
+
+export default Action
