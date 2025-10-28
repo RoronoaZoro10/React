1ed2b782dadf39b0a18694b8ce3dad22aa516183
RoronoaZoro10 <revan28710@gmail.com>
2025-10-28 22:22:55 +0530

    28oct2025

--- learning-react/src/Genre.jsx
+++ learning-react/src/Genre.jsx
@@ -1,18 +1,17 @@
+import { useState } from 'react'
 import error from './assets/error.jpg'
 
 function Genre({image = error, name = "anime genre"})
 {
-    function selected()
-    {
-        console.log("This genre is selected by",name)
-    }
+    const [selected,select] = useState(false)
 
     return(
         <>
         <div id="genres">
         <img src={image}></img>
         <h3>{name}</h3>
-        <button onClick={selected}>Select</button>
+        <button onClick={() => selected==false? select(true):select(false)}>{selected==false?"Select":"Unselect"}</button>
+        <p>{selected?"it's selected":"it's not selected"}</p>
         </div>
         </>
     )
